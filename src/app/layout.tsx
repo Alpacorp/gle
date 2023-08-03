659bbb6c48ccff6328cf1b31eafe7e54f053982b
Alpacorp <alejandro.palacios88@gmail.com>
2023-08-03 07:46:54 -0500

    add suppressHydrationWarning prop

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
-      <body className={poppins.className}>
+      <body className={poppins.className} suppressHydrationWarning>
         <Header />
         {children}
       </body>
