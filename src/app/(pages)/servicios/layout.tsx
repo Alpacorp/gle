1ef05c58bcaf3d108a21c6e6535a80dd7c30158f
Alpacorp <alejandro.palacios88@gmail.com>
2023-08-27 01:14:55 -0500

    strcuture: add main pages layouts

---
+++ src/app/(pages)/servicios/layout.tsx
@@ -0,0 +1,17 @@
+import { Metadata } from "next";
+
+import "../../globals.css";
+
+export const metadata: Metadata = {
+  title: "G.L.E. - Servicios",
+  description: "Volamos para que llegues más rápido",
+  keywords: "transporte, carga, logística, aéreo, terrestre, marítimo",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
