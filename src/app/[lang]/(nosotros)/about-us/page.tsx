7c2001b09beb74eba3cbfc3bd6c2a9ac31d20791
Alpacorp <alejandro.palacios88@gmail.com>
2023-09-26 01:43:58 -0500

    fix(Ours): add missing properties into 'en' module languge

--- src/app/[lang]/(nosotros)/about-us/page.tsx
+++ src/app/[lang]/(nosotros)/about-us/page.tsx
@@ -1,9 +1,9 @@
-import { FC } from "react";
-
 import { Ours } from "@nosotros/nosotros/container/Ours";
 
-const usPage: FC = () => {
-  return <Ours />;
+import { Locale } from "@/i18n.config";
+
+const usPage = async ({ params }: { params: { lang: Locale } }) => {
+  return <Ours lang={params.lang} />;
 };
 
 export default usPage;
