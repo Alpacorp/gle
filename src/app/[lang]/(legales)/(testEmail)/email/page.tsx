c03865ee3a9cc85abc3426a96c9649835db35b17
Alpacorp <alejandro.palacios88@gmail.com>
2023-11-04 23:58:33 -0500

    fix: delete test email files

--- src/app/[lang]/(legales)/(testEmail)/email/page.tsx
+++
@@ -1,8 +0,0 @@
-import { Locale } from "@/i18n.config";
-import { ContactEmail } from "@/src/emails/ContactEmail";
-
-const PqrsPage = async ({ params }: { params: { lang: Locale } }) => {
-  return <ContactEmail lang={params.lang} />;
-};
-
-export default PqrsPage;
