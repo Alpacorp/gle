6621dd4189cc2b84177110b5a26592681a91cc4d
Alejandro Palacios <alejandro.palacios88@gmail.com>
2023-11-09 12:14:41 -0500

    feat: configure initial redirect

--- next.config.js
+++ next.config.js
@@ -4,6 +4,15 @@ const nextConfig = {
     NEXT_PUBLIC_GA4: process.env.NEXT_PUBLIC_GA4,
     NEXT_PUBLIC_GTM: process.env.NEXT_PUBLIC_GTM,
   },
+  redirects: async () => {
+    return [
+      {
+        source: "/",
+        destination: "/es",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
