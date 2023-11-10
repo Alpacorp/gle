c305ad9ae85c6e68a9ba672f50f7da01f11c5842
Alejandro Palacios <alejandro.palacios88@gmail.com>
2023-11-09 22:25:21 -0500

    feat: fix styles and text typos

--- src/app/api/tracking/route.ts
+++ src/app/api/tracking/route.ts
@@ -3,17 +3,22 @@ import { NextResponse } from "next/server";
 export async function POST(request: Request) {
   const { remesa } = await request.json();
 
-  const res = await fetch("https://api.nmv.app/api/getUrbRemesa", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      usuario: "api_gle",
-      clave: "gle001&.",
-      remesa,
-    }),
-  });
-  const data = await res.json();
-  return NextResponse.json(data);
+  try {
+    const res = await fetch("https://api.nmv.app/api/getUrbRemesa", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        usuario: "api_gle",
+        clave: "gle001&.",
+        remesa,
+      }),
+    });
+    const data = await res.json();
+
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json({ error });
+  }
 }
